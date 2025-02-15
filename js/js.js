0c7860ff0ed75cc97255f0c34be60a216c4f78ad
ahmadkaddah <ahmd03093@gmail.com>
2025-02-15 17:29:18 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -12,20 +12,6 @@ $(window).scroll(function(){
 
 
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // منع السلوك الافتراضي
-        const targetId = this.getAttribute('href').substring(1); // الحصول على الاسم بدون #
-        const targetElement = document.getElementById(targetId); // العثور على العنصر الهدف
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' }); // التمرير إلى العنصر بسلاسة
-        }
-    });
-});
-
-
-
-
 
 
 $(document).ready(function(){
